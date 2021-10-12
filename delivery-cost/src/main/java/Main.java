84c7b51af8fcf7703c41e38953e2342b0429a8de
Tipun <mtipun@mail.ru>
2021-10-12 17:51:22 +0300

    4 lesson

--- delivery-cost/src/main/java/Main.java
+++ delivery-cost/src/main/java/Main.java
@@ -1,10 +1,32 @@
+import calc.CalcImpl;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("commit");
-        System.out.println("token");
-        System.out.println("terminal");
-        System.out.println("commit2");
-        System.out.println("terminal2");
+        //result = m*pricePerKg + km*pricePerKm;
+        //prop / json/ csv
+
+        BigDecimal weight, pricePerKg, length, pricePerKm;
+
+        try(Scanner scanner = new Scanner(System.in)) {
+            do {
+                weight = getValueFromCom(scanner, "Enter weight:");
+                pricePerKg = getValueFromCom(scanner, "Enter pricePerKg:");
+                length = getValueFromCom(scanner, "Enter km:");
+                pricePerKm = getValueFromCom(scanner, "Enter pricePerKm:");
+
+
+                System.out.println("Result: " + new CalcImpl().handle(weight,pricePerKg,length,pricePerKm));
+                System.out.println("Enter 'q' to exit and 'c' to continue");
+            }while (!scanner.hasNext("q"));
+        }
+    }
+    private static BigDecimal getValueFromCom(Scanner scanner, String outputMesage) {
+        BigDecimal enteredValue;
+        System.out.print(outputMesage);
+        enteredValue = scanner.nextBigDecimal();
+        return enteredValue;
     }
 }
