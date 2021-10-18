01a12ac47d25c4c81a1eaea7e25390c0d0499783
Tipun <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>
2021-10-18 16:45:50 +0300

    Task with interfaces

--- delivery-cost/src/main/java/Main2.java
+++ delivery-cost/src/main/java/Main2.java
@@ -1,33 +1,28 @@
 import calc.CalcImpl;
+import model.ReadData;
+import model.Status;
+import model.StatusType;
 
 import java.math.BigDecimal;
 import java.util.Scanner;
 
 public class Main2 {
     public static void main(String[] args) {
-        BigDecimal weight, pricePerKg, length, pricePerKm;
-        String close;
+        //TODO read from file
+        //pricePerKg = getValueFromCom(scanner, "Enter pricePerKg:");
+        //pricePerKm = getValueFromCom(scanner, "Enter pricePerKm:");
+
+
 
 
         try(Scanner scanner = new Scanner(System.in)) {
-            do {
-                Scanner scanner2 = new Scanner(System.in);
-                weight = getValueFromCom(scanner, "Enter weight:");
-                pricePerKg = getValueFromCom(scanner, "Enter pricePerKg:");
-                length = getValueFromCom(scanner, "Enter km:");
-                pricePerKm = getValueFromCom(scanner, "Enter pricePerKm:");
-
-                System.out.println("Result: " + new CalcImpl().handle(weight,pricePerKg,length,pricePerKm));
-                System.out.println("Enter 'q' to exit, or continue typing");
-                close = scanner2.nextLine();
-            }while (!close.equals("q"));
-            System.out.println("Ввод завершен");
+            Status currentStatus = new ReadData(scanner);
+            while (currentStatus.getType() != StatusType.EXIT) {
+                System.out.println("current type: " + currentStatus.getType());
+                currentStatus = currentStatus.handle();
+            }
         }
-    }
-    private static BigDecimal getValueFromCom(Scanner scanner, String outputMesage) {
-        BigDecimal enteredValue;
-        System.out.print(outputMesage);
-        enteredValue = scanner.nextBigDecimal();
-        return enteredValue;
+
+
     }
 }
